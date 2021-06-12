1862764229e64501bd9b3d3a9709e093e308f553
abdulfakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-12 15:19:42 -0700

    a
    a

--- application/controllers/admin/Student.php
+++ application/controllers/admin/Student.php
@@ -329,6 +329,29 @@ class Student extends CI_Controller {
         $this->load->view('admin/index', $data);
     }
 
+    public function all_student_up()
+    { 
+        $finalStudentStatus = array();
+        foreach ($this->common_model->all_studentStatus() as $key => $value) {
+            $finalStudentStatus[$value['student_id']]['db_id'] = $value['id']; 
+            $finalStudentStatus[$value['student_id']]['Status'] = $value['student_status']; 
+            $finalStudentStatus[$value['student_id']]['Status'] = ucwords(implode(' ',explode("_",$value['student_status']))); 
+            $finalStudentStatus[$value['student_id']]['formData'] = $value['formData']; 
+        }
+        $current_EmpId = $this->session->userdata('id');
+	 	$data['page_title'] = 'All Registered Students';
+        $data['studentFilter'] = 'all';
+        $data['users'] = $this->common_model->get_all_students();
+        $data['studentStatus'] = $finalStudentStatus;
+        $data['country'] = $this->common_model->select('country');
+        $data['count'] = $this->common_model->get_user_total();
+        $data['assignedTaskList'] =  $this->common_model->get_EmployeeWith_Allstudents($current_EmpId,'emp_task_assigned');
+        $data['assignedTaskListPending'] =  $this->common_model->get_EmployeeWith_Allstudents_Pending($current_EmpId,'emp_task_assigned');
+        $data['assignedTaskListComplete'] =  $this->common_model->get_EmployeeWith_Allstudents_Completed($current_EmpId,'emp_task_assigned');
+        $data['main_content'] = $this->load->view('admin/student/all_student_up', $data, TRUE);
+        $this->load->view('admin/index', $data);
+    }
+
     public function students_2020()
     { 
         $finalStudentStatus = array();
