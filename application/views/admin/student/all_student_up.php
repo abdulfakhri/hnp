3553a8a6675e88fddc47f46ba28bbf344b31688e
abdulfakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-12 15:31:51 -0700

    a
    a

--- application/views/admin/student/all_student_up.php
+++ application/views/admin/student/all_student_up.php
@@ -24,7 +24,7 @@
                           
                             <th>State</th>
                             <th>Caste</th>
-                            <th>Status</th>
+                          
                           
                            
                         </tr>
@@ -34,7 +34,7 @@
                         <?php
                         $all_scount = $all_assam_stu = $all_tripura_stu = 0;
                         foreach ($users as $user):
-                            $unserlizedData = unserialize($user['student_uploaded_data']);
+                            $unserlizedData =$user;
                             /*
                             * todo: add control condition  from employee to admin
                             */
@@ -123,85 +123,11 @@
                                         <?php echo $user['account_number']; ?>
                                     </td>
                                      <td>
-                                        <?php 
-                                        $jsondata=$user["bankstatus"];
-                                        $decodejson=json_decode($jsondata);
-                                        $full_name_db=$user['full_name'];
-                                        //$full_name_db=$user['first_name']." ".$user['last_name'];
-  
-                                        $sid=$user["student_id"];
-
-                                        $api_fetched_name=$decodejson->data->full_name;
-                                        $account_exists=$decodejson->data->account_exists;
-                                        $success=$decodejson->success;
-                                        $status_code=$decodejson->status_code;
-                                        $message_code=$decodejson->message_code;
-                                        //echo $user['api_fetched_name']; 
-                                        $full_name_db_touppercase= strtoupper($full_name_db);
-   
-                                        $api_fetched_name_touppercase= strtoupper($api_fetched_name);
-   
-                                        $full_name_db_trimed = str_replace(" ","",$full_name_db_touppercase);
-   
-                                        $api_fetched_name_trimed = str_replace(" ","",$api_fetched_name_touppercase);
-   
-                                        $full_name_db_cleaned=trim($full_name_db_trimed);
-  
-                                        $api_fetched_name_cleaned=trim($api_fetched_name_trimed);
-  
-                                       if(($full_name_db_cleaned==$api_fetched_name_cleaned) and ($account_exists==true) and ($success==true) and($status_code==200) and($message_code=="success")){
-     
-                                           $msg="Matched";
-                                           $stcolor="green";
-                                           $ncolor="blue";
-                                           $st="<p style=color:".$stcolor.">".$msg."</p>";
-                                           $final_name="<p style=color:".$ncolor.">".$api_fetched_name."</p>";
-                                           //$final_name="<p style=color:".$ncolor.">".$full_name_db."</p>";
-                                           $status=$st;
-                                           $stu= "Verified";
-      
-                                       }else if((!empty($api_fetched_name_cleaned)) and ($account_exists==true) and ($success==true) and($status_code==200) and($message_code=="success")){
-      $msg="Not Matched";
-      $stcolor="#ff00ff";
-      $ncolor="blue";
-      $st="<p style=color:".$stcolor.">".$msg."</p>";
-      $final_name="<p style=color:".$ncolor.">".$api_fetched_name."</p>";
-      //$final_name="<p style=color:".$ncolor.">".$full_name_db."</p>";
-      //$status=$final_name."<hr>".$st; 
-        $status=$st;
-      $stu= "Not Verified";
+                                 
       $query ="UPDATE students SET api_bank_st ='$stu',api_fetched_name ='$api_fetched_name' WHERE student_id='$sid'";
       mysqli_query($conn,$query);
       
-                                       }else if((empty($api_fetched_name_cleaned)==true) and ($account_exists==false) and ($success==false) and($status_code==422) and($message_code=="verification_failed")){
-      $msg="Not Availble";
-      $color="red";
-      $st="<p style=color:".$color.">".$msg."</p>";
-      $final_name="<p style=color:".$ncolor.">".$api_fetched_name."</p>";
-      $status=$st;
-      //$status="<p style=color:".$color.">".$msg."</p>";
-      
-      $stu= "Not Availble";
-      $query ="UPDATE students SET api_bank_st ='$stu',api_fetched_name ='$api_fetched_name' WHERE student_id='$sid'";
-      mysqli_query($conn,$query);
-       
-                                       }else if(empty($jsondata)==true){
-       
-      $msg="Not Verified Yet";
-      $color="brown";
-      $st="<p style=color:".$color.">".$msg."</p>";
-      //$status="<p style=color:".$color.">".$msg."</p>";
-        $status=$st;
-      
-      
-                                       }else{
-      $msg="API Balance Is Finished";
-      $color="red";
-      $status="<p style=color:".$color.">".$msg."</p>";
-      
-      
-   }
-                                       echo $status;
+                          
                                         ?>
                                     </td>
                                     <td>
@@ -215,7 +141,7 @@
                                         <?php echo $user['caste_details']; ?>
                                     </td>
                                    
-                                 <td><?php echo isset($studentStatus[$user['student_id']]['Status'])? $studentStatus[$user['student_id']]['Status'] : "Pending From Our Site"?></td>
+                                 <td><?php echo isset($studentStatus[$user['student_id']]['Status']) ? $studentStatus[$user['student_id']]['Status'] : "Pending From Our Site" ?></td>
 
                                
                             <?php } ?>
