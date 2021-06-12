0669c3829d4819d803cc97ae3b8cd364dfbf59f1
abdulfakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-12 14:57:07 -0700

    a
    a

--- application/controllers/admin/Student.php
+++ application/controllers/admin/Student.php
@@ -276,7 +276,7 @@ class Student extends CI_Controller {
 
     // all student view function
 
-    public function student_bank_verifiy($st,$yr){ 
+    public function student_bank_verifiy($yr,$st,$caste){ 
       
 
           //$st=$state;
@@ -292,7 +292,7 @@ class Student extends CI_Controller {
             $current_EmpId = $this->session->userdata('id');
             //$data['studentFilter'] = 'Tripura';
             $data['page_title'] = 'All Registered Students';
-            $data['users'] = $this->common_model->get_all_students_bank_id($st,$yr);
+            $data['users'] = $this->common_model->get_all_students_bank_id($yr,$st,$caste);
             $data['studentStatus'] = $finalStudentStatus;
             $data['country'] = $this->common_model->select('country');
             $data['count'] = $this->common_model->get_user_total();
--- application/models/Common_model.php
+++ application/models/Common_model.php
@@ -431,15 +431,15 @@ class Common_model extends CI_Model {
         return $query;
     }
 
-    function get_all_students_bank_id($yr,$st){
+    function get_all_students_bank_id($yr,$st,$caste){
        
         $this->db->select('*');
         $this->db->from('students');
         $this->db->where('is_deleted IS NULL or is_deleted <> 1');
-        if (!empty($yr) OR !empty($st)) {
+        if (!empty($yr) OR !empty($st) OR !empty($caste)) {
             $this->db->where('year', $yr);
             $this->db->where('state', $st);
-            
+            $this->db->where('caste_details', $caste);
         }
         $this->db->order_by('student_id','ASC');
         $query = $this->db->get();
