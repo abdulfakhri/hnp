881e93244a2f67b6d0e49aae08fa3021b6805a28
abdulfakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-11 17:24:27 -0700

    up
    a

--- application/models/Common_model.php
+++ application/models/Common_model.php
@@ -1,4 +1,5 @@
-<?php
+ 
+ <?php
 class Common_model extends CI_Model {
    
    
@@ -502,8 +503,9 @@ class Common_model extends CI_Model {
     function students_pending(){
         $this->db->select('*');
         $this->db->from('students');
-        $this->db->where('is_deleted IS NULL or is_deleted <> 1');
-        $this->db->where('year',"Pending");
+        //$this->db->where('is_deleted IS NULL or is_deleted <> 1');
+        $this->db->where('year ="Pending"');
+        //$this->db->where('year',"Pending");
         $query = $this->db->get();
         $query = $query->result_array();  
         return $query;
