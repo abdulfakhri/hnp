e2e20eaf123c1fce4998f9d8255cd5151bb78964
abdulfakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-14 14:57:31 -0700

    a
    a

--- api/update_status_null_value.php
+++ api/update_status_null_value.php
@@ -10,7 +10,7 @@ if(!$conn){
     die('Could not Connect MySql Server:');
 }
   
-  $result = mysqli_query($conn,"SELECT * FROM students ");  
+  $result = mysqli_query($conn,"SELECT * FROM students_n ");  
   
   if(mysqli_num_rows($result) > 0) {
    
@@ -18,12 +18,17 @@ if(!$conn){
 
       $sid=$row["student_id"];
 
-           $result1 = mysqli_query($conn,"SELECT * FROM student_status_data where student_id='$sid'");
+           $result1 = mysqli_query($conn,"SELECT * FROM student_n where student_id='$sid'");
            if (mysqli_num_rows($result1) > 0) {
                while ($ro = mysqli_fetch_array($result1)) {
 
                   $si=$ro["student_id"];
                   $st=$ro["student_status"];
+                  if($st=="null"){
+                      $status="pending_by_our_site";
+                  }else{
+                      $status=$st;
+                  }
 
                   $query ="UPDATE students_up SET student_status ='$st' WHERE student_id='$si'";
                   mysqli_query($conn,$query);
