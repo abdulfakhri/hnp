030a7ce9f12582eec3b0826b94c7dfa7f94a8885
abdulfakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-14 19:57:50 -0700

    a
    a

--- application/models/Common_model.php
+++ application/models/Common_model.php
@@ -489,6 +489,15 @@ class Common_model extends CI_Model {
         $query = $query->result_array();  
         return $query;
     }
+    function pending_by_our_site(){
+        $this->db->select('*');
+        $this->db->from('students');
+        $this->db->where('is_deleted IS NULL or is_deleted <> 1');
+        $this->db->where('student_status','Pending');
+        $query = $this->db->get();
+        $query = $query->result_array();  
+        return $query;
+    }
       
       function students_total(){
         $this->db->select('*');
