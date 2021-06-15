ed5022ae237c0bfb7d56b88b9be936bdcd80abe2
abdulfakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-14 19:12:10 -0700

    a
    a

--- application/controllers/admin/Student.php
+++ application/controllers/admin/Student.php
@@ -378,29 +378,7 @@ class Student extends CI_Controller {
         $data['main_content'] = $this->load->view('admin/student/approved_by_our_site', $data, TRUE);
         $this->load->view('admin/index', $data);
     }
-     public function  approved_by_our_site()
-    { 
-        $finalStudentStatus = array();
-        foreach ($this->common_model->all_studentStatus() as $key => $value) {
-            $finalStudentStatus[$value['student_id']]['db_id'] = $value['id']; 
-            $finalStudentStatus[$value['student_id']]['Status'] = $value['student_status']; 
-            $finalStudentStatus[$value['student_id']]['Status'] = ucwords(implode(' ',explode("_",$value['student_status']))); 
-            $finalStudentStatus[$value['student_id']]['formData'] = $value['formData']; 
-        }
-        $current_EmpId = $this->session->userdata('id');
-	 	$data['page_title'] = 'All Registered Students';
-        $data['studentFilter'] = 'all';
-        $data['users'] = $this->common_model->students_total();
-        $data['studentStatus'] = $finalStudentStatus;
-        $data['country'] = $this->common_model->select('country');
-        $data['count'] = $this->common_model->get_user_total();
-        $data['assignedTaskList'] =  $this->common_model->get_EmployeeWith_Allstudents($current_EmpId,'emp_task_assigned');
-        $data['assignedTaskListPending'] =  $this->common_model->get_EmployeeWith_Allstudents_Pending($current_EmpId,'emp_task_assigned');
-        $data['assignedTaskListComplete'] =  $this->common_model->get_EmployeeWith_Allstudents_Completed($current_EmpId,'emp_task_assigned');
-        $data['main_content'] = $this->load->view('admin/student/total_student_site', $data, TRUE);
-        $this->load->view('admin/index', $data);
-    }
-
+     
     public function deleted_student_list()
     { 
         $finalStudentStatus = array();
