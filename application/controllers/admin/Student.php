ee6ae7b39f120e3ba35113f5c9b4196343b1e5fd
abdulfakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-14 20:07:10 -0700

    a
    a

--- application/controllers/admin/Student.php
+++ application/controllers/admin/Student.php
@@ -334,7 +334,7 @@ class Student extends CI_Controller {
         $data['main_content'] = $this->load->view('admin/student/all_student', $data, TRUE);
         $this->load->view('admin/index', $data);
     }
-      public function  students_total_stie()
+      public function  students_total_site()
     { 
         $finalStudentStatus = array();
         foreach ($this->common_model->all_studentStatus() as $key => $value) {
@@ -375,6 +375,28 @@ class Student extends CI_Controller {
         $data['assignedTaskListPending'] =  $this->common_model->get_EmployeeWith_Allstudents_Pending($current_EmpId,'emp_task_assigned');
         $data['assignedTaskListComplete'] =  $this->common_model->get_EmployeeWith_Allstudents_Completed($current_EmpId,'emp_task_assigned');
         $data['main_content'] = $this->load->view('admin/student/approved_by_our_site', $data, TRUE);
+        $this->load->view('admin/index', $data);
+    }
+
+     public function  pending_by_our_site(){ 
+        $finalStudentStatus = array();
+        foreach ($this->common_model->all_studentStatus() as $key => $value) {
+            $finalStudentStatus[$value['student_id']]['db_id'] = $value['id']; 
+            $finalStudentStatus[$value['student_id']]['Status'] = $value['student_status']; 
+            $finalStudentStatus[$value['student_id']]['Status'] = ucwords(implode(' ',explode("_",$value['student_status']))); 
+            $finalStudentStatus[$value['student_id']]['formData'] = $value['formData']; 
+        }
+        $current_EmpId = $this->session->userdata('id');
+	 	$data['page_title'] = 'All Registered Students';
+        $data['studentFilter'] = 'all';
+        $data['users'] = $this->common_model->pending_by_our_site();
+        $data['studentStatus'] = $finalStudentStatus;
+        $data['country'] = $this->common_model->select('country');
+        $data['count'] = $this->common_model->get_user_total();
+        $data['assignedTaskList'] =  $this->common_model->get_EmployeeWith_Allstudents($current_EmpId,'emp_task_assigned');
+        $data['assignedTaskListPending'] =  $this->common_model->get_EmployeeWith_Allstudents_Pending($current_EmpId,'emp_task_assigned');
+        $data['assignedTaskListComplete'] =  $this->common_model->get_EmployeeWith_Allstudents_Completed($current_EmpId,'emp_task_assigned');
+        $data['main_content'] = $this->load->view('admin/student/pending_by_our_site', $data, TRUE);
         $this->load->view('admin/index', $data);
     }
      
