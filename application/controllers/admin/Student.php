903e8f9966e03e3160533b8292c39a681375e437
Abdul Fakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-16 08:58:04 -0700

    u
    u

--- application/controllers/admin/Student.php
+++ application/controllers/admin/Student.php
@@ -657,6 +657,28 @@ class Student extends CI_Controller {
         $this->load->view('admin/index', $data);
     }
     */
+    public function all_student_list()
+    { 
+        $finalStudentStatus = array();
+        foreach ($this->common_model->all_studentStatus() as $key => $value) {
+            $finalStudentStatus[$value['student_id']]['db_id'] = $value['id']; 
+            $finalStudentStatus[$value['student_id']]['Status'] = $value['student_status']; 
+            $finalStudentStatus[$value['student_id']]['Status'] = ucwords(implode(' ',explode("_",$value['student_status']))); 
+            $finalStudentStatus[$value['student_id']]['formData'] = $value['formData']; 
+        }
+        $current_EmpId = $this->session->userdata('id');
+	 	$data['page_title'] = 'All Registered Students';
+        $data['studentFilter'] = 'all';
+        $data['users'] = $this->common_model->get_all_students();
+        $data['studentStatus'] = $finalStudentStatus;
+        $data['country'] = $this->common_model->select('country');
+        $data['count'] = $this->common_model->get_user_total();
+        $data['assignedTaskList'] =  $this->common_model->get_EmployeeWith_Allstudents($current_EmpId,'emp_task_assigned');
+        $data['assignedTaskListPending'] =  $this->common_model->get_EmployeeWith_Allstudents_Pending($current_EmpId,'emp_task_assigned');
+        $data['assignedTaskListComplete'] =  $this->common_model->get_EmployeeWith_Allstudents_Completed($current_EmpId,'emp_task_assigned');
+        $data['main_content'] = $this->load->view('admin/student/all_student', $data, TRUE);
+        $this->load->view('admin/index', $data);
+    }
     public function student_bank_verifiy(){ 
         //filter_year=2020&filter_state=Tripura&filter_cast=SC&filter_status=approve_by_our_site&filter=
         
