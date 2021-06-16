8f3e799b54251e02c9692e82e8f171e151318dc3
abdulfakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-15 18:49:37 -0700

    u
    u

--- application/controllers/admin/Student.php
+++ application/controllers/admin/Student.php
@@ -17,6 +17,7 @@ class Student extends CI_Controller {
     $this->load->view('admin/student/user_view',$data);
     // $this->load->view('admin/index', $data);
    }
+   /////////////////////////////////////////////////////////////////Dashboard Boxes////////////////////////////////////
       public function total_students_our_site(){ 
          $finalStudentStatus = array();
         foreach ($this->common_model->all_studentStatus() as $key => $value) {
@@ -39,6 +40,29 @@ class Student extends CI_Controller {
         $data['main_content'] = $this->load->view('admin/student/total_student_our_site', $data, TRUE);
         $this->load->view('admin/index', $data);
     }
+     public function total_students_college_site(){ 
+         $finalStudentStatus = array();
+        foreach ($this->common_model->all_studentStatus() as $key => $value) {
+            $finalStudentStatus[$value['student_id']]['db_id'] = $value['id']; 
+            // $finalStudentStatus[$value['student_id']]['Status'] = $value['student_status']; 
+            $finalStudentStatus[$value['student_id']]['Status'] = ucwords(implode(' ',explode("_",$value['student_status']))); 
+            $finalStudentStatus[$value['student_id']]['formData'] = $value['formData']; 
+        }
+
+        $current_EmpId = $this->session->userdata('id');
+	 	$data['page_title'] = 'All Registered Students';
+        $data['studentFilter'] = 'all';
+        $data['users'] = $this->common_model->total_students_collegesite();
+         $data['studentStatus'] = $finalStudentStatus;
+        $data['country'] = $this->common_model->select('country');
+        $data['count'] = $this->common_model->get_user_total();
+        $data['assignedTaskList'] =  $this->common_model->get_EmployeeWith_Allstudents($current_EmpId,'emp_task_assigned');
+        $data['assignedTaskListPending'] =  $this->common_model->get_EmployeeWith_Allstudents_Pending($current_EmpId,'emp_task_assigned');
+        $data['assignedTaskListComplete'] =  $this->common_model->get_EmployeeWith_Allstudents_Completed($current_EmpId,'emp_task_assigned');
+        $data['main_content'] = $this->load->view('admin/student/total_student_college_site', $data, TRUE);
+        $this->load->view('admin/index', $data);
+    }
+   
    
   
    public function userDetails(){
--- application/models/Common_model.php
+++ application/models/Common_model.php
@@ -14,6 +14,23 @@ class Common_model extends CI_Model {
         $query = $query->result_array();  
         return $query;
     }
+     function total_students_collegesite(){
+        $this->db->select('*');
+        $this->db->from('students');
+        $this->db->where('is_deleted IS NULL or is_deleted <> 1');
+        $query = $this->db->get();
+        $query = $query->result_array();  
+        return $query;
+    }
+    function total_students_nspsite(){
+        $this->db->select('*');
+        $this->db->from('students');
+        $this->db->where('is_deleted IS NULL or is_deleted <> 1');
+        $query = $this->db->get();
+        $query = $query->result_array();  
+        return $query;
+    }
+    
   
     //-- insert function
     public function insert($data,$table){
