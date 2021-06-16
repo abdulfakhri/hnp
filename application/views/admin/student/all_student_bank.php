d46031400d6eb27adce7d7cabac45f78dde33914
Abdul Fakhri <40014668+abdulfakhri@users.noreply.github.com>
2021-06-16 09:03:05 -0700

    u
    u

--- application/views/admin/student/all_student_bank.php
+++ application/views/admin/student/all_student_bank.php
@@ -20,7 +20,7 @@ function myFunction() {
  <option select>Year</option>
  <option value="2020">2020</option>
  <option value="2021">2021</option>
- <option value="2019">2019</option>
+ <option value="2022">2022</option>
  <option value="Pending">Pending</option>
 </select> 
 
