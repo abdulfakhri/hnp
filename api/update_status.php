6718bde5114ef19eaf1ca63306859cbac60e8ab5
Abdul Fakhri <abf.ca@hotmail.com>
2021-06-18 17:52:28 -0700

    u

--- api/update_status.php
+++ api/update_status.php
@@ -1,56 +1,62 @@
 
-<?php 
+<?php
 //include("header.php");
 include_once 'db.php';
 $dbname = "u979436226_hnsp";
 $username = "u979436226_hnsp";
 $password = "!@#123qweasdZXC";
-$conn=mysqli_connect($host,$username,$password,$dbname);
-if(!$conn){
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (!$conn) {
     die('Could not Connect MySql Server:');
 }
-  
-  $result = mysqli_query($conn,"SELECT * FROM students ");  
-  
-  if(mysqli_num_rows($result) > 0) {
-   
-   while($row = mysqli_fetch_array($result)) {
 
-      $sid=$row["student_id"];
+$result = mysqli_query($conn, "SELECT * FROM students ");
 
-           $result1 = mysqli_query($conn,"SELECT * FROM students where student_id='$sid'");
-           if (mysqli_num_rows($result1) > 0) {
-               while ($ro = mysqli_fetch_array($result1)) {
+if (mysqli_num_rows($result) > 0) {
 
-                  $si=$ro["student_id"];
-                  $st=$ro["student_status"];
+    while ($row = mysqli_fetch_array($result)) {
 
-                  if($st=="approved_by_our_site") {
-                      $status_plain="Approved By Our Site";
-                  }elseif($st=="reject_by_our_site"){
-                      $status_plain="Reject By Our Site";
-                  }elseif($st=="approved_by_nsp"){
-                      $status_plain="Approved By NSP";
-                  }elseif($st=="approved_by_college"){
-                      $status_plain="Approved By College";
-                  }elseif($st=="rejected_by_college"){
-                      $status_plain="Reject By College";
-                  }elseif($st=="nsp_reject"){
-                      $status_plain="Reject By NSP";
-                  }elseif($st=="defect_by_our_site"){
-                      $status_plain="Defect by Our Site"; 
-                  }elseif($st=="defect_by_college"){
-                      $status_plain="Defect By College";
-                  }elseif($st=="defect_by_nsp"){
-                      $status_plain="Defect By NSP";
-                  }
+        $sid = $row["student_id"];
 
-                  $query ="UPDATE students2 SET student_status ='$st',student_st='$status_plain' WHERE student_id='$si'";
-                  mysqli_query($conn,$query);
+        $result1 = mysqli_query($conn, "SELECT * FROM students where student_id='$sid'");
+        if (mysqli_num_rows($result1) > 0) {
+            while ($ro = mysqli_fetch_array($result1)) {
 
-               }
-           }
-   }
-  } 
-  ?>
+                $si = $ro["student_id"];
+                $st = $ro["student_status"];
+
+                if ($st == "approved_by_our_site") {
+                    $status_plain = "Approved By Our Site";
+                    $sts = "approveStudents";
+                } elseif ($st == "reject_by_our_site") {
+                    $status_plain = "Reject By Our Site";
+                    $sts = "rejectStudent";
+                } elseif ($st == "approved_by_nsp") {
+                    $status_plain = "Approved By NSP";
+                    $sts = "approveStudents";
+                } elseif ($st == "approved_by_college") {
+                    $status_plain = "Approved By College";
+                    $sts = "approveStudents";
+                } elseif ($st == "rejected_by_college") {
+                    $status_plain = "Reject By College";
+                } elseif ($st == "nsp_reject") {
+                    $status_plain = "Reject By NSP";
+                    $sts = "rejectStudent";
+                } elseif ($st == "defect_by_our_site") {
+                    $status_plain = "Defect by Our Site";
+                    $sts = "defectStudents";
+                } elseif ($st == "defect_by_college") {
+                    $status_plain = "Defect By College";
+                } elseif ($st == "defect_by_nsp") {
+                    $status_plain = "Defect By NSP";
+                    $sts = "defectStudents";
+                }
+
+                $query = "UPDATE students2 SET student_status='$st',student_st='$status_plain',studentst='$sts' WHERE student_id='$si'";
+                mysqli_query($conn, $query);
+            }
+        }
+    }
+}
+?>
    
